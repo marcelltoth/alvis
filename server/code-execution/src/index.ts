4472bb7d99d1c7657180463060868941cd9b3478
Tom <pricet@Toms-MacBook-Pro.local>
2020-08-17 13:23:48 +0100

    Add pacakges

--- server/code-execution/src/index.ts
+++ server/code-execution/src/index.ts
@@ -1,9 +1,15 @@
-import './LoadEnv' // Must be the first import
-import app from '@server'
-import logger from '@shared/Logger'
+import "./LoadEnv"; // Must be the first import
+import app from "@server";
+import logger from "@shared/Logger";
 
 // Start the server
-const port = Number(process.env.PORT || 8000)
+const port = Number(process.env.PORT || 8000);
 app.listen(port, () => {
-  logger.info('Express server started on port: ' + port)
-})
+  if (process.env.NODE_ENV === "development") {
+    logger.info("Express server started on port: http://localhost:" + port);
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    logger.info("Express server started on port: " + port);
+  }
+});
